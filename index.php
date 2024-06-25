ade12329b62ba01920d31095684628de2d48dc66
MiriamPecora <miriampecora97@gmail.com>
2024-06-25 17:31:44 +0200

    [UPD] Bonus 1: modified 'movie' class to accept more than one genre

--- index.php
+++ index.php
@@ -1,48 +1,61 @@
 <?php
 
-    class Movie {
+class Movie {
 
-        private string $title;
-        private int $release;
-        private string $genre;
+    private string $title;
+    private int $release;
+    private array $genres;
 
-        public function __construct(string $title, int $release, string $genre) {
-            $this->title = $title;
-            $this->release = $release;
-            $this->setGenre($genre); 
-        }
+    public function __construct(string $title, int $release, array $genres) {
+        $this->title = $title;
+        $this->release = $release;
+        $this->setGenres($genres); 
+    }
 
-        public function getInfo(): string {
-            return "Title: " . $this->title . ", Release Year: " . $this->release . ", Genre: " . $this->genre;
-        }
+    public function getInfo(): string {
+        return "Title: " . $this->title . ", Release Year: " . $this->release . ", Genres: " . implode(", ", $this->genres);
+    }
 
-        public function setGenre(string $genre): void {
-            $allowedGenres = ["Horror", "Sci-Fi", "Drama", "Comedy", "Action"];
+    public function setGenres(array $genres): void {
+        $allowedGenres = ["Horror", "Sci-Fi", "Drama", "Comedy", "Action"];
+        foreach ($genres as $genre) {
             if (!in_array($genre, $allowedGenres)) {
                 throw new Exception("Genre not allowed. Allowed genres are: " . implode(", ", $allowedGenres) . ".");
             }
-            $this->genre = $genre;
+        }
+        $this->genres = $genres;
+    }
+
+    public function addGenre(string $genre): void {
+        $allowedGenres = ["Horror", "Sci-Fi", "Drama", "Comedy", "Action"];
+        if (!in_array($genre, $allowedGenres)) {
+            throw new Exception("Genre not allowed. Allowed genres are: " . implode(", ", $allowedGenres) . ".");
+        }
+        if (!in_array($genre, $this->genres)) {
+            $this->genres[] = $genre;
         }
     }
+}
 
-    try {
-        $movie1 = new Movie("Scream", 1996, "Horror");
-        $movie2 = new Movie("Alien", 1979, "Sci-Fi");
+try {
+    $movie1 = new Movie("Scream", 1996, ["Horror"]);
+    $movie2 = new Movie("Alien", 1979, ["Sci-Fi", "Horror"]);
 
-        echo $movie1->getInfo() . "<br>";
-        echo $movie2->getInfo() . "<br>";
-        
-    } catch (Exception $e) {
-        echo "Error: " . $e->getMessage() . "<br>";
-    }
+    echo $movie2->getInfo() . "<br>";
 
+    // Gestione aggiunta di un nuovo genere ad un film
+    $movie1->addGenre("Action");
+    echo $movie1->getInfo() . "<br>";
 
-    // Gestione dell'exception con genere non permesso
-    try {
-        $movie3 = new Movie("10 Things I Hate About You", 1999, "Romance");
-        echo $movie3->getInfo() . "<br>";
-    } catch (Exception $e) {
-        echo "Error: " . $e->getMessage() . "<br>";
-    }
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . "<br>"; 
+}
+
+try {
+    $movie3 = new Movie("10 Things I Hate About You", 1999, ["Romance"]);
+    echo $movie3->getInfo() . "<br>";
+} catch (Exception $e) {
+    echo "Error: " . $e->getMessage() . "<br>";
+}
 
 ?>
